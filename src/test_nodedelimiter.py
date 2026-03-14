1b78620efd6ab1ace2cbf1de674788afdd26b557
Eval-99 <ev1914jw@gmail.com>
2026-03-13 23:19:10 -0500

    Added test case and reverted one change, how TextNodes are printed.

--- src/test_nodedelimiter.py
+++ src/test_nodedelimiter.py
@@ -8,6 +8,12 @@ class TestSplitNodesDelimiter(unittest.TestCase):
     def test_code(self):
         node = TextNode("This is text with a `code block` word", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        expected = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+        ]
+        self.assertEqual(new_nodes, expected)
 
 
 if __name__ == "__main__":
