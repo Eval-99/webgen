76f4f19ba38d3bc12d1e4fabaf82806f43f8cd85
Eval-99 <ev1914jw@gmail.com>
2026-03-18 18:27:16 -0500

    AAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- src/extract_markdown.py
+++ src/extract_markdown.py
@@ -4,55 +4,76 @@ from textnode import TextNode, TextType
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    node_lst = []
-    for node in old_nodes:
-        if node.text_type != TextType.TEXT:
-            node_lst.append(node)
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
             continue
-
-        text_normal = ""
-        text_delimiter = ""
-        delimiter_open = False
-
-        node_words = node.text.split()
-        if node_lst != []:
-            node_words[0] = " " + node_words[0]
-
-        for word in node_words:
-            index = word
-            if word.startswith(delimiter) and delimiter_open is True:
-                raise Exception("Invalid Markdown syntax. Delimiter not closed.")
-            if word.startswith(delimiter):
-                if text_normal != "":
-                    node_lst.append(TextNode(text_normal, TextType.TEXT))
-                text_normal = ""
-                delimiter_open = True
-            if word.endswith(delimiter) and delimiter_open is False:
-                raise Exception("Invalid Markdown syntax. Delimiter not closed.")
-            if delimiter_open is True:
-                text_delimiter += word + " "
-                if word.endswith(delimiter):
-                    node_lst.append(
-                        TextNode(
-                            str(text_delimiter[:-1]).replace(delimiter, ""), text_type
-                        )
-                    )
-                    text_delimiter = ""
-                    delimiter_open = False
-                    try:
-                        node_words[node_words.index(index) + 1] = (
-                            " " + node_words[node_words.index(index) + 1]
-                        )
-                    except IndexError:
-                        continue
+        split_nodes = []
+        sections = old_node.text.split(delimiter)
+        if len(sections) % 2 == 0:
+            raise ValueError("invalid markdown, formatted section not closed")
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i], TextType.TEXT))
             else:
-                text_normal += word + " "
-        if text_normal != "":
-            node_lst.append(TextNode(text_normal[:-1], TextType.TEXT))
-        if delimiter_open is True:
-            raise Exception("Invalid Markdown syntax. Delimiter not closed.")
+                split_nodes.append(TextNode(sections[i], text_type))
+        new_nodes.extend(split_nodes)
+    return new_nodes
 
-    return node_lst
+
+# def split_nodes_delimiter(old_nodes, delimiter, text_type):
+#     node_lst = []
+#     for node in old_nodes:
+#         if node.text_type != TextType.TEXT:
+#             node_lst.append(node)
+#             continue
+#
+#         text_normal = ""
+#         text_delimiter = ""
+#         delimiter_open = False
+#
+#         node_words = node.text.split()
+#         if node_lst != []:
+#             node_words[0] = " " + node_words[0]
+#
+#         for word in node_words:
+#             index = word
+#             if word.startswith(delimiter) and delimiter_open is True:
+#                 raise Exception("Invalid Markdown syntax. Delimiter not closed.")
+#             if word.startswith(delimiter):
+#                 if text_normal != "":
+#                     node_lst.append(TextNode(text_normal, TextType.TEXT))
+#                 text_normal = ""
+#                 delimiter_open = True
+#             if word.endswith(delimiter) and delimiter_open is False:
+#                 raise Exception("Invalid Markdown syntax. Delimiter not closed.")
+#             if delimiter_open is True:
+#                 text_delimiter += word + " "
+#                 if word.endswith(delimiter):
+#                     node_lst.append(
+#                         TextNode(
+#                             str(text_delimiter[:-1]).replace(delimiter, ""), text_type
+#                         )
+#                     )
+#                     text_delimiter = ""
+#                     delimiter_open = False
+#                     try:
+#                         node_words[node_words.index(index) + 1] = (
+#                             " " + node_words[node_words.index(index) + 1]
+#                         )
+#                     except IndexError:
+#                         continue
+#             else:
+#                 text_normal += word + " "
+#         if text_normal != "":
+#             node_lst.append(TextNode(text_normal[:-1], TextType.TEXT))
+#         if delimiter_open is True:
+#             raise Exception("Invalid Markdown syntax. Delimiter not closed.")
+#
+#     return node_lst
 
 
 def extract_markdown_images(text):
@@ -97,3 +118,13 @@ def split_nodes_image(old_nodes):
         if node.text != "":
             node_lst.append(TextNode(node.text, TextType.TEXT))
     return node_lst
+
+
+def text_to_nodes(text):
+    thing = [TextNode(text, TextType.TEXT)]
+    blargus = split_nodes_delimiter(thing, "**", TextType.BOLD)
+    blargus = split_nodes_delimiter(blargus, "_", TextType.ITALIC)
+    blargus = split_nodes_delimiter(blargus, "`", TextType.CODE)
+    blargus = split_nodes_link(blargus)
+    blargus = split_nodes_image(blargus)
+    return blargus
